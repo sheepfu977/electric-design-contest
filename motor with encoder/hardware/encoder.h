90d073a0f64198360ee836a80edcc47acdfbdb75
sheepfu977 <3360523042@qq.com>
2025-05-26 20:39:02 +0800

    2025/5/26

---
+++ motor with encoder/hardware/encoder.h
@@ -0,0 +1,8 @@
+#ifndef ENCODER_H
+#define ENCODER_H
+
+#include <main.h>
+#include <stdio.h>
+int read_speed(TIM_HandleTypeDef *htim);
+
+#endif
---
+++ motor with encoder/hardware/motor.h
@@ -0,0 +1,9 @@
+#ifndef MOTOR_H
+#define MOTOR_H
+
+#include <main.h>
+#include <stdio.h>
+void motor_setspeed(int motor1);
+
+#endif
+
